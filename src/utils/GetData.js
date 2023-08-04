bf837f6167fb1becd37050eef1a132142a73e2d0
jojoner16 <laybats.jonathan@orange.fr>
2023-08-04 17:17:59 +0200

    création du dossier utils avec les fichiers callapi, getdata et mockeddata

---
+++ src/utils/GetData.js
@@ -0,0 +1,34 @@
+/* eslint-disable default-case */
+import {
+  getUserActivity,
+  getUserAverageSessions,
+  getUserInfos,
+  getUserPerformance,
+} from './MockedData';
+
+/**
+ *
+ * @param {string} type
+ * @param {number} id
+ * @returns {Array}
+ */
+
+export const getData = async (type, id) => {
+  let data = [];
+
+  switch (type) {
+    case 'USER_ACTIVITY':
+      data = await getUserActivity(id);
+      break;
+    case 'USER_PERFORMANCE':
+      data = await getUserPerformance(id);
+      break;
+    case 'USER_AVERAGE_SESSIONS':
+      data = await getUserAverageSessions(id);
+      break;
+    case 'USER_MAIN_DATA':
+      data = await getUserInfos(id);
+      break;
+  }
+  return data;
+};
