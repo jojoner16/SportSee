e840739e8139d100a46bb14c0b42cbeee3eec0e4
jojoner16 <laybats.jonathan@orange.fr>
2023-10-05 16:17:17 +0200

    déplacement du composant sidebar dans app.js et modification css

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import User from '../pages/user/User';
 import Header from './header/Header';
+import SideBar from '../components/sideBar/SideBar';
 
 const App = () => {
   // useEffect permet de modifier le titre de la page
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Header />
+      <SideBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user/:userId" element={<User />} />
