3e3b2a58b5ff90237af4aa1a2cf54056c9794604
jojoner16 <laybats.jonathan@orange.fr>
2024-01-05 11:39:14 +0100

    variable environnement et page erreur

---
+++ src/components/error/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div>
+      <h2>Le serveur est inaccessible</h2>
+      <p>
+        Désolé, nous n'avons pas pu nous connecter au serveur. Veuillez
+        réessayer plus tard.
+      </p>
+    </div>
+  );
+}
+
+export default Error;
